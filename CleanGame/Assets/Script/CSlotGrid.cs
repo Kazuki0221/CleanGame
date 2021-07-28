a0924a3921e621d72df2fbc8ea7ee53a12891d8f
kazu0221 <kirito-sao.com3290@ezweb.ne.jp>
2021-07-28 10:45:03 +0900

    オリジナルデータコミット

---
+++ CleanGame/Assets/Script/CSlotGrid.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class CSlotGrid : MonoBehaviour
+{
+    [SerializeField] private GameObject[] slotPrefab;
+    private int slotNumber = 3;
+
+    [SerializeField] CItem[] allItem = new CItem[3];//所持アイテム
+    int index;
+
+    PlayerControl pc;
+    GameManager gm;
+
+    void Start()
+    {
+        pc = GetComponent<PlayerControl>();
+        gm = GetComponent<GameManager>();
+    }
+
+    private void Update()
+    {
+        //GetItem();
+        for (int i = 0; i < slotNumber; i++)//取得したアイテムをインベントリに表示
+        {
+
+            CSlot slot = slotPrefab[i].GetComponent<CSlot>();
+
+            if (i < allItem.Length)
+            {
+                slot.SetItem(allItem[i]);
+            }
+            else
+            {
+                slot.SetItem(null);
+            }
+        }
+    }
+
+    //public void GetItem()//アイテムスロットに取得したアイテムを保持
+    //{
+    //    for (int i = 0; i < slotNumber; i++)
+    //    {
+    //        if (gm.item[i].ItemName == pc.GetItemName())
+    //        {
+    //            allItem[index] = gm.item[i];
+    //            if (index < 2)
+    //            {
+    //                index++;
+    //            }
+    //        }
+    //    }
+    //}
+
+
+}
